7199857f9f753b6d9ed105eeafbe45ce2184942d
masset151 <masset.ruiz151@gmail.com>
2021-09-20 13:09:57 +0200

    mi primer respositorio

---
+++ src/main/java/com/everis/everisdesafiomvn_iniciales/App.java
@@ -0,0 +1,21 @@
+package com.everis.everisdesafiomvn_iniciales;
+import java.lang.reflect.*;
+
+import org.apache.commons.lang3.StringUtils;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    
+    
+    {
+    	String holaMundo = "Hola Mundo";
+    	
+        System.out.println(StringUtils.capitalize("hello world!") );
+        System.out.println(StringUtils.isEmpty(holaMundo));
+        
+    }
+}
